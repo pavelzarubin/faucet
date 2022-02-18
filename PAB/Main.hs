4990fd603aa55a54dd6e4a3df65c55c7169a1bcf
Paul <paffel2@gmail.com>
2022-02-18 16:04:03 +0300

    add PAB

---
+++ PAB/Main.hs
@@ -0,0 +1,5 @@
+module Main where
+
+import PAB
+
+main = runSimulator
--- PAB/PAB.hs
+++ PAB/PAB.hs
@@ -4,12 +4,13 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module PAB where
+module PAB (runSimulator) where
 
 import Control.Monad (void)
 import Control.Monad.Freer (interpret)
@@ -44,3 +45,35 @@ instance Builtin.HasDefinitions FaucetContracts where
   getContract = \case
     Init sp -> SomeBuiltin $ startFaucet @() sp
     Grab fp -> SomeBuiltin $ grabWithError @() fp
+
+handlers :: SimulatorEffectHandlers (Builtin FaucetContracts)
+handlers =
+  Simulator.mkSimulatorHandlers def def $
+    interpret (contractHandler Builtin.handleBuiltin)
+
+runSimulator :: IO ()
+runSimulator = void $
+  Simulator.runSimulationWith handlers $ do
+    Simulator.logString @(Builtin FaucetContracts) "Starting plutus-starter PAB webserver on port 8080. Press enter to exit."
+    shutdown <- PAB.Server.startServerDebug
+
+    let wallet1 = knownWallet 1
+        wallet2 = knownWallet 2
+        startParams =
+          StartParams
+            { newAmount = 10_000_000,
+              newDat = FaucetDatum 123 456
+            }
+    void $ Simulator.activateContract wallet1 $ Init startParams
+
+    Simulator.waitNSlots 1
+
+    void $ Simulator.activateContract wallet2 $ Grab (FaucetParams 123)
+
+    void $ Simulator.waitNSlots 1
+
+    Simulator.logString @(Builtin FaucetContracts) "Balances at the end of the simulation"
+    b <- Simulator.currentBalances
+    Simulator.logBalances @(Builtin FaucetContracts) b
+
+    shutdown
